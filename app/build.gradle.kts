8a5de42af25e29589a1605280b5e3817eaeba4f9
Bohdan Liashuk <luashuk2002@gmail.com>
2023-10-27 15:14:12 +0300

    Change text and sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildToolsVersion = "33.0.1"
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
